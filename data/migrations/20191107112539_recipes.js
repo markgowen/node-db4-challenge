b1f67cdd7b39964f46906747be6ea34e7f6a6f68
Mark Gowen <mark.gowen78@gmail.com>
2019-11-07 11:52:30 -0800

    created recipes migration
    created recipes migration with table data

--- data/migrations/20191107112539_recipes.js
+++ data/migrations/20191107112539_recipes.js
@@ -1,8 +1,38 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('recipes', tbl => {
+      tbl.increments();
+      tbl.string('recipe_name', 255).notNullable();
+      tbl
+        .integer('ingredient')
+        .notNullable()
+        .unsigned()
+        .references('ingredient_name')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+      tbl.float('ingredient_qty', 64).notNullable();
+      tbl
+        .integer('step_instructions', 255)
+        .unsigned()
+        .references('steps')
+        .inTable('steps')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+    })
+    .createTable('ingredient', tbl => {
+      tbl.increments();
+      tbl.string('name', 255).notNullable();
+    })
+    .createTable('steps', tbl => {
+      tbl.increments();
+      tbl.integer('step').notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('steps')
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('recipes');
 };
