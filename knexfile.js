d17ba627bb8c492f7f33240e601eeb2535fa87c4
Mark Gowen <mark.gowen78@gmail.com>
2019-11-07 11:57:30 -0800

    migrated latest
    created db

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data.recipe-db.db3'
+      filename: './data/recipe-db.db3'
     },
     useNullAsDefault: true,
     migrations: {
